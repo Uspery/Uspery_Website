a8e6e8ede9d70936ae7a1c047dc3a796a2139b69
Kaua Lima <kauavitorioof@gmail.com>
2023-07-08 11:25:25 -0300

    Adicao de imagem

--- public/team/src/script/index.js
+++ public/team/src/script/index.js
@@ -40,40 +40,98 @@ function replaceAgeInText(text, age) {
 addEventListener("load", (event) => {});
 onload = (event) => {
     GetJson(function(jsonData) {
-      if (jsonData) {
-        // Função para gerar a descrição com base nos dados do JSON
-        function generateDescription(data) {
-            var sigla = data["sigla"];
-            var descriptionElement = document.getElementById("desc-" + sigla);
-            if (descriptionElement) {
-              var age = Calculate_age(data["dateOfBirth"]);
-              var replacedText = replaceAgeInText(data["description"], age);
-              descriptionElement.innerHTML = replacedText;
+        if (jsonData) {
+            // Função para criar os itens no HTML
+            function createItems(data) {
+                var sigla = data["sigla"];
+              
+                // Criação da seção de criador
+                var section = document.createElement("section");
+                section.classList.add("section-cards");
+                section.id = sigla;
+              
+                // Criação do elemento de cartão
+                var card = document.createElement("div");
+                card.classList.add("card", "about");
+              
+                // Criação da coluna de texto
+                var columnText = document.createElement("div");
+                columnText.classList.add("column-text", "about");
+              
+                // Criação da imagem
+                var image = document.createElement("img");
+                image.classList.add("member-img");
+                image.src = data["imageUrl"];
+                image.alt = data["name"];
+              
+                // Criação do contêiner para o título com a imagem e o nome do usuário
+                var titleContainer = document.createElement("div");
+                titleContainer.classList.add("container-title");
+              
+                // Criação do título do criador
+                var title = document.createElement("h2");
+                title.classList.add("title-card");
+                var userName = document.createTextNode(data["name"]);
+                title.appendChild(userName); // Adiciona o nome do usuário ao título
+              
+                // Adiciona o título ao contêiner do título
+                titleContainer.appendChild(title);
+
+                if(data["imageUrl"]) // Adiciona a imagem ao contêiner do título caso tenha imagem
+                    titleContainer.appendChild(image); 
+              
+                // Criação da descrição
+                var description = document.createElement("h3");
+                description.classList.add("text-card-semi");
+                description.id = "desc-" + sigla;
+              
+                // Criação do link "Know more"
+                var link = document.createElement("h3");
+                link.classList.add("text-card-semi");
+                var anchor = document.createElement("a");
+                anchor.href = data["url"]; // Usando a propriedade "url" como valor do atributo "href"
+                anchor.classList.add("spam-card");
+                anchor.textContent = "Know more";
+                // link.appendChild(document.createTextNode("Know more: "));
+                link.appendChild(anchor);
+              
+                // Adicionar os elementos à estrutura HTML
+                columnText.appendChild(titleContainer); // Adiciona o contêiner do título à coluna de texto
+                columnText.appendChild(description);
+                columnText.appendChild(link);
+                card.appendChild(columnText);
+                section.appendChild(card);
+              
+                // Encontrar a seção de criadores e inserir a nova seção gerada
+                var teamSection = document.getElementById("team");
+                if (teamSection) {
+                  teamSection.appendChild(section);
+                }
+              
+                // Chamar a função generateDescription
+                generateDescription(data);
+              }
+              
+              
+
+            // Função para gerar a descrição com base nos dados do JSON
+            function generateDescription(data) {
+                var sigla = data["sigla"];
+                var descriptionElement = document.getElementById("desc-" + sigla);
+                if (descriptionElement) {
+                    var age = Calculate_age(data["dateOfBirth"]);
+                    var replacedText = replaceAgeInText(data["description"], age);
+                    descriptionElement.innerHTML = replacedText;
+                }
             }
-          }
 
-        // Iterar sobre cada objeto do JSON
-        jsonData.forEach(function(person) {
-          generateDescription(person);
-        });
-      } else {
-        console.error("Erro ao obter os dados JSON.");
-      }
+            // Iterar sobre cada objeto do JSON e criar os itens no HTML
+            jsonData.forEach(function(person) {
+                createItems(person);
+            });
+        }
+        else {
+            console.error("Erro ao obter os dados JSON.");
+        }
     });
-  };
-
-// var desc_yuri = document.getElementById("desc-yuri-dantas");
-// desc_yuri.innerHTML = `"Hi!
-//                         My name is Yuri Dantas, I'm `+ yuri_age +` years old, I'm from Goiânia-GO, I'm CEO and Copydesk at Uspery. I'm  graduated in basic information technology. I have a passion for writing, and in 2021 I started my first written project."`
-
-// var desc_pedro = document.getElementById("desc-pedro-henrique");
-// desc_pedro.innerHTML = `"Hi!
-//                         My name is Pedro Henrique, I'm `+ pedro_age +` years old, I'm CEO and Systems Analyst at Uspery.  I'm graduated in Systems Development from ETEC."`
-
-// var desc_leonardo = document.getElementById("desc-leonardo-ieva");
-// desc_leonardo.innerHTML = `"Hi!
-//                         My name is Leonardo Ieva, I'm `+ leonardo_age +` years old, I'm Web Developer Junior at Uspery. I'm styding Science Computer from Impacta."`
-
-// var desc_carol = document.getElementById("desc-carol-oliveira");
-// desc_carol.innerHTML = `"Hi!
-//                          My name is Caroline Oliveira, I'm ` + carol_age + ` years old, I'm Back-end Developer at Uspery. I'm graduand in Systems Development at ETEC and I'm styding Computer Science at Universidade São Judas Tadeu."`
+};
