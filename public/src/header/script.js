46055964eab5d84d0b9e210fffa20384c47e10ae
Kaua Lima <kauavitorioof@gmail.com>
2023-07-20 21:58:16 -0300

    Novas informacoes e layout

--- public/src/header/script.js
+++ public/src/header/script.js
@@ -1,11 +1,13 @@
 var privacy_btn = document.getElementById('privacy_btn')
 var about_btn = document.getElementById('about_btn')
+var team_btn = document.getElementById('team_btn')
 var contact_btn = document.getElementById('contact_btn')
 var logo_btn = document.getElementById('logo_btn')
 
 //  Url types
 var privacy_EN = ["privacy", "privacidade"]
 var about_EN = ["about", "sobre", ""]
+var team_EN = ["team", "time", ""]
 var contact_EN = ["contact", "contato"]
 
 var proc_and_host = window.location.protocol + "//" + window.location.host
@@ -26,24 +28,32 @@ contact_btn.addEventListener("click", () => {
     window.top.location.href = proc_and_host + getInstance() + "contact/";
 })
 
+team_btn.addEventListener("click", () => {
+    window.top.location.href = proc_and_host + getInstance() + "team/";
+})
+
 function getInstance(){
-    if(proc_and_host.includes('localhost') || proc_and_host.includes('5500')
-    || proc_and_host.includes('5501')) return "/public/"
-    else return "/";
+    if(proc_and_host.includes('localhost')  || proc_and_host.includes('5501')) 
+        return "/public/"
+    else
+        return "/";
 }
 
 //  This function will be called when the page is loaded
 //  In the onload method
-function checkLoc(){
+function DefineLocBackground(){
     var path = replacePublic(document.referrer);
-    if(privacy_EN.includes(path)){
+    if(privacy_EN.includes(path))
         privacy_btn.style.backgroundColor = "var(--black)"
 
-    }else if(about_EN.includes(path)){
+    else if(about_EN.includes(path))
         about_btn.style.backgroundColor = "var(--black)"
-    } else if(contact_EN.includes(path)) {
+        
+    else if(contact_EN.includes(path)) 
         contact_btn.style.backgroundColor = "var(--black)"
-    }
+        
+    else if(team_EN.includes(path)) 
+        team_btn.style.backgroundColor = "var(--black)"
 }
 
 function replacePublic(url){
